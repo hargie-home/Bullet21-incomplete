7c5d9d8ba13b7db8974519d89276ad7fe68e0e6a
hargie-home <72682239+hargie-home@users.noreply.github.com>
2020-11-10 11:06:50 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,48 @@
+var bullet, wall;
+var speed, weight, thickness;
+
+function setup() {
+  createCanvas(1500,400);
+  
+  thickness = random(22,83);
+  speed = random(223,321);
+  weight = random(30,52);
+
+  bullet = createSprite(50, 200, 10, 5);
+  bullet.velocityX = speed;
+  bullet.shapeColor = "white";
+
+  wall = createSprite(1200, 200, thickness, height/2);
+  wall.shapeColor = "white";
+  
+}
+
+function draw() {
+  background(0);
+  
+  if (hasCollide(bullet,  wall)) {
+    bullet.velocityX = 0;
+    var damage = 0.5 * bulletWeight * bulletSpeed * bulletSpeed/(thickness * thickness * thickness);
+
+    if(damage > 10) {
+      wall.shapeColor = "red";
+    }
+
+   
+    if(damage < 10) {
+      wall.shapeColor = "green";
+    }
+ }
+    
+  hasCollided();
+  drawSprites();
+} 
+
+function hasCollided(bullet, wall) {
+  bulletRightEdge = lbullet.x + lbullet.width;
+  wallLeftedge = lwall.x;
+  if (bulletRightEdge >= wallLeftEdge) {
+    return true
+  }
+  return false;
+}
